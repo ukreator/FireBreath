e51315d84514a738b05a42cdb165eadc14df4f56
Tadeusz Kozak <stoiczek@gmail.com>
2011-04-22 15:36:41 +0200

    Removed options WITH_SYSTEM_BOOST and WITH_DYNAMIC_MSVC_RUNTIME if variables already defined

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -7,5 +7,11 @@ endif(NOT FB_PROJECTS_DIR)
 set(FB_CONFIG_DIR "${FB_ROOT}/src/config" CACHE STRING "Location to find global project configuration files (.h files, etc)" )
 
 option(BUILD_EXAMPLES "Build example projects" OFF)
-option(WITH_DYNAMIC_MSVC_RUNTIME "Build with dynamic MSVC runtime (/MD)" OFF)
-option(WITH_SYSTEM_BOOST "Build with system Boost" OFF)
+
+# Vars that can be set by cmake that includes FB scripts
+if(NOT DEFINED WITH_DYNAMIC_MSVC_RUNTIME)
+  option(WITH_DYNAMIC_MSVC_RUNTIME "Build with dynamic MSVC runtime (/MD)" OFF)
+endif()
+if(NOT DEFINED WITH_SYSTEM_BOOST )
+  option(WITH_SYSTEM_BOOST "Build with system Boost" OFF)
+endif()
